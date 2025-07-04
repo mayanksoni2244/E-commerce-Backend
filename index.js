35f0b355865fc3be06bc814470bae97b5454f828
mayank soni <ms9218764@gmail.com>
2025-07-04 11:48:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import mongoose, { Schema } from "mongoose";
 import server from './routes/authroutes.js';
 import cors from 'cors';
 import router from './routes/orderRouter.js'
-import dotenv from 'dotenv';
-dotenv.config()
 
 const app = express();
 app.use(express.json());
@@ -13,8 +11,7 @@ app.use(cors());
 app.use('/api', router);
 
 
-const port =process.env.PORT || 3000;
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://ms9218764:mayank123@cluster0.fivajcs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>{
     console.log('connected to database')
 })
@@ -46,6 +43,6 @@ const User=mongoose.model('User',userSchema)
 export default User;
 
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
+app.listen(3000,()=>{
+    console.log(`server is running on port 3000`)
 })
